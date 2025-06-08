2948a774e821a5a06e11c93d7109acd41d27f3c6
Mikmame <81294326+Mikmame@users.noreply.github.com>
2025-06-08 09:21:16 +0300

    Map attribution update

--- src/helpers/add-tile-layer.js
+++ src/helpers/add-tile-layer.js
@@ -1,13 +1,25 @@
 import L from "leaflet";
-
+let currentTileLayer;
 export function addTileLayer(map) {
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+   if (currentTileLayer) {
+    map.removeLayer(currentTileLayer);
+  }
+
+  currentTileLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-   attribution: `
-  <a href="https://leafletjs.com">Leaflet</a> |
-  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a> |
-  Coded by <a href="https://github.com/Mikmame/ip-address-tracker" target="_blank">Mikmame</a> |
-  &copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>
-`,
+    attributionControl: false 
   }).addTo(map);
+
+  
+  L.control.attribution({
+    prefix: false 
+  }).addTo(map);
+
+  map.attributionControl.setPrefix(false); 
+
+  map.attributionControl.addAttribution(`
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a> |
+    Coded by <a href="https://github.com/Mikmame/ip-address-tracker" target="_blank">mikmame</a> |
+    &copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>
+  `);
 }
